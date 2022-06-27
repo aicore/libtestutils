99016c13b8be114c7e5ab538bab332aa7754a773
charly abraham <charly@core.ai>
2022-06-27 16:59:53 +0530

    test: added integragion tests

---
+++ test/integration/install-test.spec.js
@@ -0,0 +1,17 @@
+/*global describe, it*/
+import * as chai from 'chai';
+import {install_mysql, uninstall_mysql} from "../../src";
+
+let expect = chai.expect;
+
+describe('integration tests for libtestsUtils',function (){
+    it('install mysql test', async function () {
+        const retVal = await install_mysql();
+        expect(retVal.length).gt(0);
+    });
+    it('uninstall mysql test', async function () {
+        const retVal = await uninstall_mysql();
+        expect(retVal.length).gt(0);
+    });
+
+});
