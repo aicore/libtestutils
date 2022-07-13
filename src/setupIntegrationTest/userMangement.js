3244aa661d2b514021f5f1a1c782095a4ec26bb9
charly abraham <charly@core.ai>
2022-07-13 14:17:27 +0530

    feat: adding support db setup for integration tests

---
+++ src/setupIntegrationTest/userMangement.js
@@ -0,0 +1,19 @@
+import {execShell} from "../utils/shellUtils.js";
+import {isString} from "@aicore/libcommonutils";
+
+export async function setUpMySQL(configs) {
+    if (!isString(configs.database)) {
+        throw new Error('Please provide valid Database Name');
+    }
+    if (!isString(configs.user)) {
+        throw new Error('Please provide valid Database user');
+    }
+    if (isString(configs.password)) {
+        throw new Error('Please provide valid password for database');
+    }
+    await execShell(`sudo mysql -u root -e 'CREATE DATABASE ${configs.database};'`);
+    await execShell(
+        `sudo mysql -u root -e "CREATE USER '${configs.user}'@'localhost' IDENTIFIED BY '${configs.password}'";`);
+    await execShell(`sudo mysql -u root -e "GRANT ALL ON ${configs.database}.* TO '${configs.user}'@'localhost'";`);
+    await execShell(`sudo mysql -u root -e "FLUSH PRIVILEGES;";`);
+}
