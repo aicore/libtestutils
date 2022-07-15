a26f216477c53411fe8440bfb6f9ab73cb43f4a4
charly abraham <charly@core.ai>
2022-07-15 18:19:49 +0530

    feat: code cleanup

--- test/integration/install-test.spec.js
+++ test/integration/install-test.spec.js
@@ -1,15 +1,14 @@
 /*global describe, it*/
 import * as chai from 'chai';
 import {installMysql, setUpMySQL, uninstallMysql} from "../../src/index.js";
-import {getMySqlConfigs} from "@aicore/libmysql/src/utils/configs.js";
+
 let expect = chai.expect;
 
 describe('integration tests for libtestsUtils', function () {
     it('Setup database should pass', async function () {
         try {
-            const configs = getMySqlConfigs();
-            console.log(JSON.stringify(configs));
-            await setUpMySQL(configs);
+            const configs = await setUpMySQL();
+            console.log(`${JSON.stringify(configs)}`);
         } catch (e) {
             console.log(e.toString());
         }
