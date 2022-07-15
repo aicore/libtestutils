d48eb959bdd5021108cbf298016f7a83c54fbd1e
charly abraham <charly@core.ai>
2022-07-15 12:39:24 +0530

    feat: install and configure mysql

--- test/integration/install-test.spec.js
+++ test/integration/install-test.spec.js
@@ -8,11 +8,9 @@ import {execShell} from "../../src/utils/shellUtils.js";
 let expect = chai.expect;
 
 describe('integration tests for libtestsUtils',function (){
-   /* describe('install test', function (){
+    describe('install test', function (){
         it('install mysql test', async function () {
             const retVal = await installMysql();
-            const pwd= await execShell('grep -oP \'temporary password(.*): \\K(\\S+)\' /var/log/mysqld.log');
-            console.log(`pwd is ${pwd}`);
             expect(retVal.length).gt(0);
         });
         it('uninstall mysql test', async function () {
@@ -22,23 +20,22 @@ describe('integration tests for libtestsUtils',function (){
 
     });
 
-    */
     it('Setup database should pass', async function () {
-        //const retVal = await installMysql();
+        const retVal = await installMysql();
         try {
-            //const mysqlStatus = await isMySqlActive();
-            // console.log(mysqlStatus);
-            //const activateStatus = await startMySql();
-           // console.log(activateStatus);
-          //  expect(retVal.length).gt(0);
-            const configs = getMySqlConfigs();
+           const mysqlStatus = await isMySqlActive();
+           console.log(mysqlStatus);
+           const activateStatus = await startMySql();
+           console.log(activateStatus);
+//           expect(retVal.length).gt(0);
+           const configs = getMySqlConfigs();
             console.log(JSON.stringify(configs));
             await setUpMySQL(configs);
 
         } catch (e) {
             console.log(e.toString());
         }
-      //  await uninstallMysql();
+      await uninstallMysql();
     });
 
 
