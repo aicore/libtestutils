ffbc792a209376c2c1ae2a8dc5d3fee5ec09d4b0
charly abraham <charly@core.ai>
2022-07-15 12:07:26 +0530

    fix: msql github action

--- src/setupIntegrationTest/userMangement.js
+++ src/setupIntegrationTest/userMangement.js
@@ -11,9 +11,9 @@ export async function setUpMySQL(configs) {
     if (!isString(configs.password)) {
         throw new Error('Please provide valid password for database');
     }
-    await execShell(`sudo mysql -u root -p='' -e 'CREATE DATABASE ${configs.database};'`);
+    await execShell(`mysql -uroot -h127.0.0.1 -e 'CREATE DATABASE ${configs.database};'`);
     await execShell(
-        `sudo mysql -u root -e -p='' "CREATE USER '${configs.user}'@'localhost' IDENTIFIED BY '${configs.password}'";`);
-    await execShell(`sudo mysql -u root -p='' -e "GRANT ALL ON ${configs.database}.* TO '${configs.user}'@'localhost'";`);
-    await execShell(`sudo mysql -u root -p='' -e "FLUSH PRIVILEGES;";`);
+        `mysql -uroot -h127.0.0.1 "CREATE USER '${configs.user}'@'localhost' IDENTIFIED BY '${configs.password}'";`);
+    await execShell(`mysql -uroot -h127.0.0.1 -e "GRANT ALL ON ${configs.database}.* TO '${configs.user}'@'localhost'";`);
+    await execShell(`mysql -uroot -h127.0.0.1 -e "FLUSH PRIVILEGES;";`);
 }
