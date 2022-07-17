1bc033809813aaf0a6391972ecc7949e8a1bf384
charly abraham <charly@core.ai>
2022-07-17 20:58:12 +0530

    fix: sudo permission missiing

--- src/setupIntegrationTest/userMangement.js
+++ src/setupIntegrationTest/userMangement.js
@@ -8,14 +8,14 @@ import {getMySqlConfigs} from "@aicore/libcommonutils";
  **/
 export async function setUpMySQL() {
     const configs = getMySqlConfigs();
-    let cmd = `mysql -uroot -h127.0.0.1 -e 'CREATE DATABASE ${configs.database};'`;
+    let cmd = `sudo mysql -uroot -h127.0.0.1 -e 'CREATE DATABASE ${configs.database};'`;
     await execShell(cmd);
     cmd =
-        `mysql -uroot -h127.0.0.1 -e  "CREATE USER '${configs.user}'@'localhost' IDENTIFIED BY '${configs.password}'";`;
+        `sudo mysql -uroot -h127.0.0.1 -e  "CREATE USER '${configs.user}'@'localhost' IDENTIFIED BY '${configs.password}'";`;
     await execShell(cmd);
-    cmd = `mysql -uroot -h127.0.0.1 -e "GRANT ALL ON ${configs.database}.* TO '${configs.user}'@'localhost'";`;
+    cmd = `sudo mysql -uroot -h127.0.0.1 -e "GRANT ALL ON ${configs.database}.* TO '${configs.user}'@'localhost'";`;
     await execShell(cmd);
-    cmd = `mysql -uroot -h127.0.0.1 -e "FLUSH PRIVILEGES";`;
+    cmd = `sudo mysql -uroot -h127.0.0.1 -e "FLUSH PRIVILEGES";`;
     await execShell(cmd);
     return configs;
 }
